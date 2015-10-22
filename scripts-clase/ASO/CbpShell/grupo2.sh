68e7ec61e1ea699c1b0eef18a6716ee93032cea0
Lybert <felixgs2@hotmail.com>
2015-10-23 00:10:37 +0200

    Update grupo2.sh

--- scripts-clase/ASO/CbpShell/grupo2.sh
+++ scripts-clase/ASO/CbpShell/grupo2.sh
@@ -10,3 +10,20 @@ echo "_________________________________________________________"
 	cat /etc/group | cut -d: -f 1,3,4 | sort -k 2 -t ":" | more -13
 echo "---------------------------------------------------------"
 read -p "Presione cualquier tecla para continuar" pausa
+
+
+#Prueba esta estructura:
+
+#!/bin/bash
+
+clear
+
+echo "ID_Grupo / Grupo / Usuarios"
+echo "_________________________________________________________"
+	cat /etc/group | cut -d':' -f1,3,4 | tr ':' ' ' | sort -nk 2 | more -13
+
+echo "<--------------------------------------------------------->"
+
+read -p "Presione INTRO para continuar" pausa
+
+#xLy
